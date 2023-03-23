d990d47f120090db1c3b2b32b8a246b22f9d9b89
k_terada <k_terada@yumemi.co.jp>
2023-03-23 11:05:14 +0900

    Initial Commit

---
+++ SwiftUIEditListExample/SwiftUIEditListExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIEditListExampleApp.swift
+//  SwiftUIEditListExample
+//
+//  Created by Katsuhiko Terada on 2023/03/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIEditListExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
